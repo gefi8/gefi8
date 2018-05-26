7815466447ff2b3566b3ae129f7f17d26b5ae62b
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-05-26 17:32:16 -0300

    Add responsive menu, some style changes and directory organization

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,15 @@ window.onscroll = function onScroll(event) {
     if (element.getBoundingClientRect().y <= 10 &&
       element.getBoundingClientRect().height + windowHeight >= windowHeight &&
       element.getBoundingClientRect().y * (-1) <= element.getBoundingClientRect().height - 10) {
-      document.querySelector(`nav ul li a[href="#${sectionId}"]`).setAttribute('class', 'active');
+      document.querySelector(`div.side-menu nav ul li a[href="#${sectionId}"]`).setAttribute('class', 'active');
     } else {
-      document.querySelector(`nav ul li a[href="#${sectionId}"]`).setAttribute('class', 'deactivated');
+      document.querySelector(`div.side-menu nav ul li a[href="#${sectionId}"]`).setAttribute('class', 'deactivated');
     }
   });
+}
+
+function hideMenu() {
+  let menuCheckbox = document.querySelector('.menu-mobile .menu-btn');
+  menuCheckbox.click();
+  console.log(menuCheckbox);
 }
